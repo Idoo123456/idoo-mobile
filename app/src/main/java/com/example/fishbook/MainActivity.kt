ae24fc89cdf4bd4f72787bf67082b345ab337a47
lab151-33 <miftahulrizki204@gmail.com>
2026-04-08 16:28:25 +0700

    Commit Mobile Day 4

--- app/src/main/java/com/example/fishbook/MainActivity.kt
+++ app/src/main/java/com/example/fishbook/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.fishbook
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fishbook.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnToFourth = findViewById<Button>(R.id.btnToFourth)
+
+        btnToFourth.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("name", "Politeknik Caltex Riau")
+            intent.putExtra("from", "Rumbai")
+            intent.putExtra("age", 25)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/fishbook/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,65 @@
+package com.example.fishbook.pertemuan_4
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fishbook.databinding.ActivityFourthBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+
+class FourthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFourthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFourthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.e("onCreate", "FourthActivity dibuat pertama kali")
+
+        // Mengambil data yang dikirim dari MainActivity
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age", 0)
+
+        // Menampilkan data di Logcat
+        Log.e("Data Intent", "Nama: $name , Usia: $age, Asal: $from")
+
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+
+        binding.btnShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Tutup") {
+                    Log.e("Info Snackbar", "Snackbar ditutup")
+                }
+                .show()
+        }
+
+        binding.btnShowAlertDialog.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog", "Anda memilih Ya!")
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog", "Anda memilih Tidak!")
+                }
+                .show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("onStart", "onStart: FourthActivity terlihat di layar")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("onDestroy", "FourthActivity dihapus dari stack")
+    }
+}
